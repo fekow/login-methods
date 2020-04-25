0dd9cfc4dc689825210293ed11c84958f0a0cbb5
Fernando Binkowski <binkowskideandrade@gmail.com>
2020-04-25 17:53:09 -0300

    added md5 to passwords

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose")
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5")
 
 const app = express();
 
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ["password"]}) // uses secret as an encryptor by using the mongoose-encryption as a plugin to the desired schema, and selecting only one field
+// userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ["password"]}) // uses secret as an encryptor by using the mongoose-encryption as a plugin to the desired schema, and selecting only one field
 
 const User = new mongoose.model("User", userSchema)
 
@@ -44,7 +44,7 @@ app.get("/register", (req,res)=>{
 app.post("/register", (req,res)=>{
     const user = new User({
         email: req.body.username,
-        password: req.body.password 
+        password: md5(req.body.password)
     })
     user.save((err)=>{
         if(err){
@@ -57,7 +57,7 @@ app.post("/register", (req,res)=>{
 
 app.post("/login",(req,res)=>{
     const username = req.body.username
-    const password = req.body.password
+    const password = md5(req.body.password)
 
     User.findOne({ 
         email: username}, 
