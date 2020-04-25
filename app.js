47c8810ec8d78f1f63e3ff9f423ea697db376d63
Fernando Binkowski <binkowskideandrade@gmail.com>
2020-04-25 18:26:26 -0300

    installed bcrypt with salting

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose")
-const md5 = require("md5")
+const bcrypt = require("bcrypt")
+const saltRounds = 10;
 
 const app = express();
 
@@ -42,34 +43,42 @@ app.get("/register", (req,res)=>{
     res.render("register")
 })
 app.post("/register", (req,res)=>{
-    const user = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
-    user.save((err)=>{
-        if(err){
-            res.send(err)
-        } else {
-            res.render("secrets")
-        }
-    })
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const user = new User({
+            email: req.body.username,
+            password: hash
+        })
+        user.save((err)=>{
+            if(err){
+                res.send(err)
+            } else {
+                res.render("secrets")
+            }
+        }) 
+    });
+
+
 })
 
-app.post("/login",(req,res)=>{
+app.post("/login", (req, res) => {
     const username = req.body.username
-    const password = md5(req.body.password)
+    const password = req.body.password
 
-    User.findOne({ 
-        email: username}, 
-        (err, foundUser)=>{
+    User.findOne({email: username},
+        (err, foundUser) => {
             if (err) {
                 console.log(err);
-            } else{
-                if (foundUser.password === password) {
-                    res.render("secrets")
+            } else {
+                if (foundUser) {
+                    bcrypt.compare(password, foundUser.password, function (err, result) {
+                        if (result) {
+                            res.render("secrets")
+                        }
+                    });
                 }
             }
-    })
+        })
 })
 
 app.listen(1337,()=>{
